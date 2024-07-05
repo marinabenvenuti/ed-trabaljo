9ad40ef6f85320332111c456d980cb6234058e9f
marinabenvenuti <marinabcardeal@gmail.com>
2024-07-05 00:55:37 -0300

    carga de dados+busca+exibir livros

--- interface.py
+++ interface.py
@@ -10,7 +10,7 @@ class Interface():
         print('1 - Inclusão de livro')
         print('2 - Busca de livro')
         print('3 - Exclusão de livro')
-        print('4 - Exibir livros')
+        print('4 - Exibir todos os livros')
         print('5 - Carga de dados')
         print('0 - Finalizar sistema')
         print()
@@ -25,3 +25,57 @@ class Interface():
         print()
 
         return nome, autor, genero, preco
+    
+    def opcoes_busca(self):
+        print()
+        print('Escolha uma opção de busca: ')
+        print('1 - Buscar por nome')
+        print('2 - Buscar por autor')
+        print('3 - Buscar por gênero')
+        print('4 - Buscar por intervalo de preço')
+        print('5 - Buscar por autor e gênero')
+        print('6 - Buscar por autor e intervalo de preço')
+        print('7 - Buscar por gênero e intervalo de preço')
+        print('0 - Voltar')
+        print()
+        opcao = input('Digite a opção: ')
+        return opcao
+    
+    def opcoes_preco(self):
+        print()
+        print('Escolha um intervalo de preço')
+        print('1 - Até R$30,00')
+        print('2 - Entre R$30 e R$50')
+        print('3 - Entre R$50 e R$100')
+        print('4 - Entre R$100 e R$200')
+        print('5 - Acima de R$200')
+        print()
+        intervalo = input('Escolha um intervalo: ')
+        return intervalo
+    
+    def busca_basica(self, tipo):
+        tipo = input(f'Digite o {tipo} do livro: ')
+        return tipo
+    
+    def print_livro(self, livro):
+        print()
+        print(f'Livro: {livro.nome}')
+        print(f'Autor: {livro.autor}')
+        print(f'Gênero: {livro.genero}')
+        print(f'Preço: {livro.preco}')
+        print('-------------------------------------')
+
+    def print_basico(self, conteudo):
+        print()
+        print(conteudo)
+        print()
+
+    def exibir_livros(self, livros):
+        if not livros:
+            print("Não há livros cadastrados.")
+        else:
+            print("{:<5} {:<25} {:<25} {:<15} {:<10}".format("ID", "Nome", "Autor", "Gênero", "Preço"))
+            print("="*80)
+            for livro in livros:
+                print("{:<5} {:<25} {:<25} {:<15} R${:<10.2f}".format(livro.id, livro.nome, livro.autor, livro.genero, livro.preco))
+            print("="*80)
--- lista_invertida.py
+++ lista_invertida.py
@@ -1,3 +1,4 @@
+import random
 from interface import Interface
 from livro import Livro
 from item_discreto_diretorio import ItemDiscretoDiretorio
@@ -10,6 +11,17 @@ class ListaInvertida():
         self.__livros = []
         self.__interface = Interface()
 
+    def teste(self):
+        for autores in self.__autores:
+            print(autores.item)
+        for generos in self.__generos:
+            print(generos.item)
+        print(self.__precos['30'])
+        print(self.__precos['50'])
+        print(self.__precos['100'])
+        print(self.__precos['200'])
+        print(self.__precos['acima'])
+    
     def inicializa_sistema(self):
         exec = True
         while exec:
@@ -17,61 +29,58 @@ class ListaInvertida():
 
             if opcao == '1':
                 inclusao = self.incluir_livro()
-                print(inclusao)
+                self.__interface.print_basico(inclusao)
             
             elif opcao == '2':
-                pass
+                self.busca_geral()
 
             elif opcao == '3':
-                pass
+                self.teste()
 
             elif opcao == '4':
-                pass
+                if self.__livros == []:
+                    self.__interface.print_basico('Sem livros cadastrados')
+                else:
+                    self.__interface.exibir_livros(self.__livros)
 
             elif opcao == '5':
-                pass
+                self.carga_de_dados()
 
             elif opcao == '0':
                 exec = False
 
     def intervalo_preco(self, livro):
         if livro.preco < 30.00:
-            self.__precos['30'].append(livro)
+            self.__precos['30'].append(livro.id)
         
         elif livro.preco > 30.00 and livro.preco < 50.00:
-            self.__precos['50'].append(livro)
+            self.__precos['50'].append(livro.id)
         
         elif livro.preco > 50.00 and livro.preco < 100.00:
-            self.__precos['100'].append(livro)
+            self.__precos['100'].append(livro.id)
         
         elif livro.preco > 100.00 and livro.preco < 200.00:
-            self.__precos['200'].append(livro)
+            self.__precos['200'].append(livro.id)
         
         elif livro.preco > 200.00:
             self.__precos['acima'].append(livro)
 
-    def incluir_livro(self):
-        infos_livro = self.__interface.incluir_livro()
-
-        for livro in self.__livros:
-            if infos_livro[0] == livro.nome and infos_livro[1] == livro.autor and infos_livro[2] == livro.genero:
-                return('Este livro já está cadastrado!')
-            
-        livro = Livro(0, infos_livro[0], infos_livro[1], infos_livro[2], infos_livro[3])
-        self.__livros.append(livro)
-
+    def gerar_id(self):
+        return random.randint(0, 100000)
+    
+    def incluir_nos_diretorios(self, livro, cod):
         # adicionando autor ou id novo no diretório
         if self.__autores != []:
             cont = 0
             for autor in self.__autores:
                 if autor.item == livro.autor:
-                    autor.add_id(0)
+                    autor.add_id(cod)
                     cont += 1
             if cont == 0:
-                autor = ItemDiscretoDiretorio(livro.autor, 0)
+                autor = ItemDiscretoDiretorio(livro.autor, cod)
                 self.__autores.append(autor)
         else:
-            autor = ItemDiscretoDiretorio(livro.autor, 0)
+            autor = ItemDiscretoDiretorio(livro.autor, cod)
             self.__autores.append(autor)
 
         # adicionando genero ou id novo no diretório
@@ -79,17 +88,169 @@ class ListaInvertida():
             cont = 0
             for genero in self.__generos:
                 if genero.item == livro.genero:
-                    genero.add_id(0)
+                    genero.add_id(cod)
                     cont += 1
             if cont == 0:
-                genero = ItemDiscretoDiretorio(livro.genero, 0)
+                genero = ItemDiscretoDiretorio(livro.genero, cod)
                 self.__generos.append(genero)
         else:
-            genero = ItemDiscretoDiretorio(livro.genero, 0)
+            genero = ItemDiscretoDiretorio(livro.genero, cod)
             self.__generos.append(genero)
 
         # adicionando preço
         self.intervalo_preco(livro)
+
+
+    def incluir_livro(self):
+        infos_livro = self.__interface.incluir_livro()
+
+        for livro in self.__livros:
+            if infos_livro[0] == livro.nome and infos_livro[1] == livro.autor and infos_livro[2] == livro.genero:
+                return 'Este livro já está cadastrado!'
+            
+        cod = self.gerar_id()
+        livro = Livro(cod, infos_livro[0], infos_livro[1], infos_livro[2], infos_livro[3])
+        self.__livros.append(livro)
+
+        self.incluir_nos_diretorios(livro, cod)
     
         return f'Livro {livro.nome} adicionado ao sistema'
     
+    def busca_geral(self):
+        while True:
+            opcao_busca = self.__interface.opcoes_busca()
+
+            if opcao_busca == '1':
+                nome = self.__interface.busca_basica('nome')
+                encontrados = [livro for livro in self.__livros if livro.nome == nome]
+                if encontrados:
+                    for livro in encontrados:
+                        self.__interface.print_livro(livro)
+                else:
+                    self.__interface.print_basico('Livro não encontrado')
+
+            elif opcao_busca == '2':
+                result = self.busca_autor()
+                if result == 'Autor não encontrado':
+                    self.__interface.print_basico('Autor não encontrado')
+                else:
+                    self.__interface.print_basico('Livros com o autor informado:')
+                    self.livro_por_id(result)
+
+            elif opcao_busca == '3':
+                result = self.busca_genero()
+                if result == 'Gênero não encontrado':
+                    self.__interface.print_basico('Gênero não encontrado')
+                else:
+                    self.__interface.print_basico('Livros com o gênero informado:')
+                    self.livro_por_id(result)
+
+            elif opcao_busca == '4':
+                result = self.busca_preco()
+                if result == []:
+                    self.__interface.print_basico('Sem livros neste intervalo')
+                else:
+                    self.__interface.print_basico('Livros com o intervalo de preço informado: ')
+                    self.livro_por_id(result)
+
+            elif opcao_busca == '5':
+                result_autor = self.busca_autor()
+                result_genero = self.busca_genero()
+                if result_autor == 'Autor não encontrado' or result_genero == 'Gênero não encontrado':
+                    self.__interface.print_basico('O gênero e/ou o autor informado não está cadastrado. Tente novamente')
+                else:
+                    self.__interface.print_basico('Livro(s) com o autor e gênero informado')
+                    encontrados = [livro for livro in self.__livros if livro.id in result_autor and livro.id in result_genero]
+                    if encontrados:
+                        for livro in encontrados:
+                            self.__interface.print_livro(livro)
+                    else:
+                        self.__interface.print_basico('Nenhum livro encontrado com o autor e gênero informado')
+
+            elif opcao_busca == '6':
+                result_autor = self.busca_autor()
+                result_preco = self.busca_preco()
+                if result_autor == 'Autor não encontrado' or result_preco == []:
+                    self.__interface.print_basico('O autor e/ou o intervalo de preço informado não está cadastrado. Tente novamente')
+                else:
+                    self.__interface.print_basico('Livro(s) com o autor e intervalo de preço informado')
+                    encontrados = [livro for livro in self.__livros if livro.id in result_autor and livro in result_preco]
+                    if encontrados:
+                        for livro in encontrados:
+                            self.__interface.print_livro(livro)
+                    else:
+                        self.__interface.print_basico('Nenhum livro encontrado com o autor e intervalo de preço informado')
+
+            elif opcao_busca == '7':
+                result_genero = self.busca_genero()
+                result_preco = self.busca_preco()
+                if result_genero == 'Gênero não encontrado' or result_preco == []:
+                    self.__interface.print_basico('O gênero e/ou o intervalo de preço informado não está cadastrado. Tente novamente')
+                else:
+                    self.__interface.print_basico('Livro(s) com o gênero e intervalo de preço informado')
+                    encontrados = [livro for livro in self.__livros if livro.id in result_genero and livro in result_preco]
+                    if encontrados:
+                        for livro in encontrados:
+                            self.__interface.print_livro(livro)
+                    else:
+                        self.__interface.print_basico('Nenhum livro encontrado com o gênero e intervalo de preço informado')
+
+            elif opcao_busca == '8':
+                break
+
+    def livro_por_id(self, result):
+        for livro in self.__livros:
+            for id in result:
+                if livro.id == id:
+                    self.__interface.print_livro(livro)
+    
+    def busca_autor(self):
+        autor = self.__interface.busca_basica('autor')
+        for autores in self.__autores:
+            if autor == autores.item:
+                return autores.ids
+        return 'Autor não encontrado'
+        
+
+    def busca_genero(self):
+        genero = self.__interface.busca_basica('gênero')
+        for generos in self.__generos:
+            if genero == generos.item:
+                return generos.ids
+        return 'Gênero não encontrado'
+
+    def busca_preco(self):
+        preco = self.__interface.opcoes_preco()
+        if preco == '1':
+            return self.__precos['30']
+        if preco == '2':
+            return self.__precos['50']
+        if preco == '3':
+            return self.__precos['100']
+        if preco == '4':
+            return self.__precos['200']
+        if preco == '5':
+            return self.__precos['acima']
+        
+    def carga_de_dados(self):
+        livros_exemplo = [
+            ("Dom Casmurro", "Machado de Assis", "Romance", 40.00),
+            ("Memórias Póstumas de Brás Cubas", "Machado de Assis", "Romance", 50.00),
+            ("O Pequeno Príncipe", "Antoine de Saint-Exupéry", "Infantil", 35.00),
+            ("O Senhor dos Anéis", "J.R.R. Tolkien", "Fantasia", 80.00),
+            ("Harry Potter e a Pedra Filosofal", "J.K. Rowling", "Fantasia", 45.00),
+            ("1984", "George Orwell", "Ficção Científica", 30.00),
+            ("Orgulho e Preconceito", "Jane Austen", "Romance", 40.00),
+            ("A Revolução dos Bichos", "George Orwell", "Fábula", 35.00),
+            ("Cem Anos de Solidão", "Gabriel García Márquez", "Realismo Mágico", 55.00),
+            ("Crime e Castigo", "Fiódor Dostoiévski", "Romance", 60.00),
+        ]
+
+        for infos_livro in livros_exemplo:
+            id_aleatorio = self.gerar_id()
+            livro = Livro(id_aleatorio, infos_livro[0], infos_livro[1], infos_livro[2], infos_livro[3])
+            self.__livros.append(livro)
+
+            self.incluir_nos_diretorios(livro, id_aleatorio)
+        
+        self.__interface.print_basico("Carga de dados concluída! 10 livros adicionados ao sistema.")
